e3dd945f05e2f907ba7be4545ebfb6238a39f354
DiegoSPB <diego.spb@gmail.com>
2020-05-09 20:26:15 -0700

    simplify

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Link from 'next/link'
+
+const Index = () => (
+  <>
+    <h1>
+      hello
+    </h1>
+    <Link href="/resume"><a>resume</a></Link>
+  </>
+)
+
+export default Index
---
+++ pages/resume.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Link from 'next/link'
+
+const Resume = () => (
+  <>
+    <h1>
+      resume
+    </h1>
+    <Link href="/"><a>resume</a></Link>
+  </>
+)
+
+export default Resume
