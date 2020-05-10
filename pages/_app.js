7b2719d4e9fc3285b7399a2a8b83441b7c8ce6df
DiegoSPB <diego.spb@gmail.com>
2020-05-09 21:16:37 -0700

    more styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import './global.css'
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle =  createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+      sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -12,13 +12,54 @@ const Container = styled.div`
   min-height: calc(100vh - 5rem);
   padding: 1rem;
 `;
+const Title = styled.h1`
+  align-items: baseline;
+  border-bottom: 1px solid;
+  display: -webkit-inline-box;
+  display: -webkit-inline-flex;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  font-size: 1.25rem;
+  font-weight: 300;
+  justify-content: space-between;
+  line-height: 1;
+  min-width: 15rem;
+  padding-bottom: 1.25rem;
+  width: 100%;
+`;
+const Name = styled.span`
+    display: inline-block;
+    margin-left: 3rem;
+`;
+
+const Links = styled.p`
+  font-weight: 300;
+  margin-bottom: 1rem;
+  margin-top: 0;
+  text-align: right;
+
+  a {
+    font-weight: 500;
+    text-decoration: none;
+    transition: color 0.4s;
+    :hover {
+      color: #0074d9;
+    }
+    :visited {
+      color: inherit;
+    }
+  }
+`;
 
 const Index = () => (
   <Container>
-    <h1>
-      hello
-    </h1>
-    <Link href="/resume"><a>resume</a></Link>
+    <Title>
+      <Name>Diego SPB</Name>
+      <Name>Diego SPB</Name>
+    </Title>
+    <Links>
+      <Link href="/resume"><a>resume</a></Link>
+    </Links>
   </Container>
 )
 
